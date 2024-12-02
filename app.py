9bb6343461c8f995ce387aed4a8052bb06ce7bfb
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-12-01 20:14:00 -0500

    Retrieving adventofcode headers and decrypting

--- app.py
+++ app.py
@@ -11,11 +11,12 @@ class AESCipher:
         self.key = key
 
     def decrypt(self, text):
-        cipher = AES.new(self.key, AES.MODE_CBC, IV=(' ' * 16))
+        cipher = AES.new(self.key, AES.MODE_CBC, IV=(b' ' * 16))
         return self._unpad(cipher.decrypt(text))
 
     def _unpad(self, s):
-        return s[:-ord(s[len(s) - 1:])]
+        padding_length = s[-1]
+        return s[:-padding_length]
 
 
 passwd = keyring.get_password('Chrome Safe Storage', 'Chrome')
@@ -32,9 +33,7 @@ cookie_file = os.path.expanduser(
     '~/Library/Application Support/Google/Chrome/Default/Cookies')
 
 conn = sqlite3.connect(cookie_file)
-
-# NOTE: Chrome uses Win32_FILETIME format
-# NOTE: 11644473600 == strftime('%s', '1601-01-01')
+conn.text_factory = bytes
 sql = 'SELECT host_key,path,is_secure,name,value,encrypted_value,((expires_utc/1000000)-11644473600) FROM cookies'
 
 if __name__ == '__main__':
@@ -43,13 +42,16 @@ if __name__ == '__main__':
 
         value = _value
         if encrypted_value[:3] == b'v10':
-            encrypted_value = encrypted_value[3:]  # Trim prefix 'v10'
+            encrypted_value = encrypted_value[3:]
             value = cipher.decrypt(encrypted_value)
-            value = value.decode()
+            value = value[32:].decode('utf-8')
 
         exptime = max(_exptime, 0)
         secure = str(bool(is_secure)).upper()
 
-        print(host_key, 'TRUE', path, secure, exptime, name, value, sep='\t')
+        if "adventofcode" in host_key.decode("utf-8"):
+            print(host_key, 'TRUE', path, secure, exptime, name, value, sep='\t')
+
 
     conn.rollback()
+
